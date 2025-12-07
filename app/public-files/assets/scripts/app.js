c38f22470df419c49ec887c4e851c5c6739b7d97
Halfmann-LT <tylholman06@gmail.com>
2025-12-07 15:57:43 +0000

    progress on js for form handling

--- app/public-files/assets/scripts/app.js
+++ app/public-files/assets/scripts/app.js
@@ -637,11 +637,50 @@ function loadWaterContent () {
 /* ---------- Signup page ---------- */
 
 function loadSignupContent () {
-  console.log("Loading Sign Up Content...")
+    console.log("Loading Sign Up Content...")
 
-  // Build your page through javascript here
+    // Build your page through javascript here
+}
+
+let signUpForm = document.querySelector('form');
+let firstNameInput = document.querySelector('#firstName');
+let lastNameInput = document.querySelector('#lastName');
+let emailInput = document.querySelector('#email');
+let commentInput = document.querySelector('#comments');
+let confirmMessage = document.querySelector('#confirmMessage');
+
+if (signUpForm) {
+    signUpForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        confirmMessage.textContent = signUpForm.submitText;
+
+        const formBody = {
+            firstName: firstNameInput.value,
+            lastName: lastNameInput.value,
+            email: emailInput.value,
+            comment: commentInput.value
+        };
+        const requestHeaders = {
+            'Content-Type': 'application/json',
+        };
+
+        fetch('/sign-up', {
+            method: 'post',
+            headers: requestHeaders,
+            body: JSON.stringify(formBody)
+        })
+            .then((response) => {
+                return response.json();
+            })
+            .then((responsedata) => {
+                console.log(responsedata);
+                confirmMessage.textContent = `Hi ${responsedata.firstName} ${responsedata.lastName} , your comments have been
+                received and we will contact you at ${responsedata.email} shortly`;
+            });
+    })
 
 }
+
 
 /* ---------- Signup page ---------- */
 
